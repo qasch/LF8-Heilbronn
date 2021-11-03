a2779f6096914a69b30a2d54ebf9c0bf9ebdbe0e
kschell <kai.schell@lpice.eu>
2021-11-03 13:32:20 +0100

    Add second page and more routing

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,5 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def hello():
     return "<h1>Hallo Flask :)</h1>"
+
+@app.route("/huhu")
+def huhu():
+    return "<h1>Huhu Du.</h1>"
