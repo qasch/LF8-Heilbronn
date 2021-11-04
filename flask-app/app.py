d045c983de6b8a5dd919b83337e56ccef27f3433
kschell <kai.schell@lpice.eu>
2021-11-04 14:36:02 +0100

    Modify app.py for db support

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
-
 @app.route("/")
 @app.route("/index")
-def hello():
-    return "<h1>Hallo Flask :)</h1>"
+def index():
+    connection = sqlite3.connect('db/test.db')
+    cursor = connection.cursor()
 
-@app.route("/huhu")
-def huhu():
-    return "Huhu Du"
+    # Select Statement ausführen
+    # fetchall() sorgt dafür, dass das Ergebnis in einer Liste gespeichert wird
+    rows = cursor.execute(""" SELECT * FROM person; """).fetchall()
+
+    # Änderungen übermitteln
+    connection.commit()
+    # Verbindung zur Datenbank schliessen
+    connection.close()
+
+    return render_template('index.html', rows=rows)
 
 @app.route("/grid")
 def grid():
     return render_template('grid.html')
+
+if __name__ == '__main__':
+    app.run()
